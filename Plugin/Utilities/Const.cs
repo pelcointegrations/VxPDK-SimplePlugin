026ffaa138c19cc9a21693b9b97f7364557b01ec
Brad Lackey <blackey@gmail.com>
2017-01-20 16:21:19 -0700

    Update Const.cs
    Added the email where users can request a plugin key

--- Plugin/Utilities/Const.cs
+++ Plugin/Utilities/Const.cs
@@ -14,7 +14,7 @@ namespace PluginNs.Utilities
         public static readonly string LogFilePath = Path.Combine(Directory.GetParent(Assembly.GetCallingAssembly().Location).FullName,
             "TestPlugin.txt");
         public static readonly string PluginId = "__CREATE_UUID__";
-        public static readonly string PluginKey = "__ASK_PELCO_REPRESENTITIVE__";
+        public static readonly string PluginKey = "__ASK_PELCO_REPRESENTITIVE_partnerfirst@schneider-electric.com__";
 
         public static readonly string VxSdkLogFilePath = Directory.GetParent(Assembly.GetCallingAssembly().Location).FullName;
         public static readonly LogLevel.Value VxSdkLogLevel = LogLevel.Value.Debug;
