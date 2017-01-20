d8927b9f2c2ddcca4c1d7bb907125417c817865c
Bradley Lackey <brad.lackey@schneider-electric.com>
2017-01-20 16:12:02 -0700

    Updated sdk key directions

--- Plugin/Utilities/Const.cs
+++ Plugin/Utilities/Const.cs
@@ -18,7 +18,7 @@ namespace PluginNs.Utilities
 
         public static readonly string VxSdkLogFilePath = Directory.GetParent(Assembly.GetCallingAssembly().Location).FullName;
         public static readonly LogLevel.Value VxSdkLogLevel = LogLevel.Value.Debug;
-        public static readonly string VxSdkKey = "__INSTALL_VXSDK_AND_GENERATE_VXSDK_KEY_pdn.pelco.com__";
+        public static readonly string VxSdkKey = "__GENERATE_VXSDK_KEY_FROM_External\Pelco\VxSdk-1.2\Tools\VxSdkKeyGen.exe__";
 
         public static readonly string PersistentModel = "PersistentModel";
         public static readonly string RegionMainView = "RegionMainView";
